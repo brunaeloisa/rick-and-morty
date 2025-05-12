766b4c63839b8a036377c4f0766a50dc86e4526d
bruna <brunaeloisa7@gmail.com>
2025-05-12 13:54:24 -0300

    pagination

--- js/index.js
+++ js/index.js
@@ -20,6 +20,7 @@ async function loadMainContent(page) {
   }
 
   renderCharactersList(characters);
+  renderPagination(result.prevPage, result.nextPage);
 }
 
 async function renderFooterData() {
@@ -34,6 +35,48 @@ async function renderFooterData() {
   document.getElementById('current-year').innerText = currentYear;
 }
 
+function renderPagination(prevPage, nextPage) {
+  const prevPageNumber = prevPage ? prevPage.split('?page=')[1] : 0;
+  const nextPageNumber = nextPage ? nextPage.split('?page=')[1] : 0;
+
+  const nav = document.getElementById('pagination');
+  nav.innerHTML = '';
+
+  const ul = document.createElement('ul');
+  ul.classList.add('pagination', 'justify-content-center', 'm-0');
+
+  const liPrevPage = document.createElement('li');
+  liPrevPage.classList.add('page-item');
+
+  if (!prevPage) liPrevPage.classList.add('disabled');
+
+  const buttonPrev = document.createElement('button');
+  buttonPrev.setAttribute('type', 'button');
+  buttonPrev.classList.add('page-link', 'btn-page');
+  buttonPrev.innerText = 'Anterior';
+  buttonPrev.addEventListener('click', () => loadMainContent(prevPageNumber));
+
+  liPrevPage.appendChild(buttonPrev);
+
+  const liNextPage = document.createElement('li');
+  liNextPage.classList.add('page-item');
+
+  if (!nextPage) liNextPage.classList.add('disabled');
+
+  const buttonNext = document.createElement('button');
+  buttonNext.setAttribute('type', 'button');
+  buttonNext.classList.add('page-link', 'btn-page');
+  buttonNext.innerText = 'Próxima';
+  buttonNext.addEventListener('click', () => loadMainContent(nextPageNumber));
+
+  liNextPage.appendChild(buttonNext);
+
+  ul.appendChild(liPrevPage);
+  ul.appendChild(liNextPage);
+
+  nav.appendChild(ul);
+}
+
 function renderCharactersList(characters) {
   const row = document.getElementById('characters-list');
   row.innerHTML = '';
@@ -42,17 +85,17 @@ function renderCharactersList(characters) {
     const cardHTML = `
       <div class="card overflow-hidden h-100">
         <div class="row g-0 h-100">
-          <div class="mobile-w col-5 col-xl-4">
+          <div class="mobile-w col-5 col-sm-4 col-lg-5 col-xl-4">
             <img src="${character.image}" class="img-fluid object-fit-cover h-100 w-100" alt="Foto do personagem ${character.name}">
           </div>
 
-          <div class="mobile-w col-7 col-xl-8">
+          <div class="mobile-w col-7 col-sm-8 col-lg-7 col-xl-8">
             <div class="card-body fw-bold text-white">
               <h5 class="card-title fw-bold mb-1">${character.name}</h5>
 
               <p class="card-text status">
                 <i id="circle-status" class="bi bi-circle-fill text-${mapStatus(character.status).color}"></i>
-                <span>${mapStatus(character.status).text} - ${mapSpecies(character.species)}</span>
+                <span>${mapStatus(character.status).text} – ${mapSpecies(character.species)}</span>
               </p>
 
               <p class="card-text">
@@ -107,7 +150,9 @@ function mapSpecies(species) {
       return 'Robô';
     case 'unknown': 
       return 'Espécie desconhecida';
+    case 'Mythological Creature':
+      return 'Criatura Mitológica';
     default: 
-      return `Outro (${species})`;
+      return species;
   }
 }
